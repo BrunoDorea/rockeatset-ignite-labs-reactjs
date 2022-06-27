2789c5c49ec7c2543a362c02491a1152eeb573d9
Bruno Henrique Félix Dórea <brunodorea@outlook.com.br>
2022-06-27 09:11:41 -0300

    commit

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,3 +1,4 @@
+import { ArrowCircleRight } from "phosphor-react";
 import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
@@ -17,7 +18,15 @@ export function Event() {
       <main className="flex flex-1">
         { slug 
         ? <Video lessonSlug={slug}/> 
-        : <div className="flex-1"></div>
+        : (
+          <div className='flex-1 pt-[40vh] text-gray-300 text-xl'>
+            <span className='flex flex-row items-center justify-center gap-2'>
+              Clique em uma aula para começar
+              <ArrowCircleRight size={24} weight="light" className='animate-pulse text-green-500' />
+            </span>
+            
+          </div>
+        )
         }
         <Sidebar />
       </main>
